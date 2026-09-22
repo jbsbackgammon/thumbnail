ea9925bbb72fda1bb739705ee483d7778c98d811
jbsbackgammon <jbsbackgammon@gmail.com>
2026-09-23 04:00:55 +0900

    Remove legacy players from master

--- players.js
+++ players.js
@@ -1,17 +1,2 @@
-// 選手マスター
-// 追加する場合は assets/players/ に512x512pxの画像を置き、下の配列に1件追加してください。
-// photo は index.html から見た相対パスです。
-window.JBS_PLAYERS = [
-  {
-    id: 'yanagi-nobusuke',
-    name: '柳 暢祐',
-    roman: 'YANAGI Nobusuke',
-    photo: 'assets/players/yanagi_nobusuke.png'
-  },
-  {
-    id: 'meijo-kentaro',
-    name: '名城 健太郎',
-    roman: 'MEIJO Kentaro',
-    photo: 'assets/players/meijo_kentaro.png'
-  }
-];
+// 選手マスタは assets/players/ の写真ファイル名から players.json に自動生成します。
+window.JBS_PLAYERS = [];
